7b18fc03a2c7d70c4e8b451bb0a5a2f5b7266e3a
Halina <galaratkova@gmail.com>
2019-11-08 11:05:28 -0500

    first commit

---
+++ Assign2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Assign2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] myArray = new int[] { 1, 2, 3, 1, 1,3,3,3,5 };
+            int[] frequency = new int[myArray.Length];
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                int counter = 1;
+
+                for (int j =i+1; j < myArray.Length; j++)
+                {
+                    if (myArray[i] == myArray[j])
+                    {
+                        counter++;
+                        frequency[j] = -1;
+                    }
+                }
+                if (frequency[i] != -1)
+                {
+                    frequency[i] = counter;
+                }
+            }
+
+            for (int k = 0; k < frequency.Length; k++)
+            {
+                if (frequency[k] != -1)
+                    Console.WriteLine("Array element   " + myArray[k] + "    |    " + "Frequency " + frequency[k]);
+            }
+             Console.ReadLine();
+        }
+    }
+}
+
+
